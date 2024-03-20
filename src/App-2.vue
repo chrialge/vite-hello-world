e2cb20c7da9cabead20c1b37aaf4b67b6b7fce32
chrialge <chrialge@gmail.com>
2024-03-20 15:27:36 +0100

    add repo scalffolding with two component

---
+++ src/App-2.vue
@@ -0,0 +1,19 @@
+<script>
+export default{
+  data(){
+    return{
+      text: 'Hello vue vite'
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{text}}</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default{
+  data(){
+    return{
+      text: 'I am second component'
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{text}}</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
